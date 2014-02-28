c493880a907630b4f5b4015dfaa22c2fb92f11be
sjw0010 <sjw0010@auburn.edu>
2014-02-28 17:17:12 -0600

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+	has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  	has_many :followers, through: :reverse_relationships, source: :follower
 	before_create :create_remember_token
 	before_save { self.email = email.downcase }
 
@@ -16,9 +22,20 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end
+    Micropost.from_users_followed_by(self)
+end
+
+def following?(other_user)
+	self.relationships.find_by(followed_id: other_user.id)
+end
+
+def follow!(other_user)
+	self.relationships.create!(followed_id: other_user.id)
+end
+
+def unfollow!(other_user)
+    self.relationships.find_by(followed_id: other_user.id).destroy
+end
 
 def User.new_remember_token
 	SecureRandom.urlsafe_base64
