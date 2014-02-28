482334fb03f985ca24b756bdff6db569721dd398
sjw0010 <sjw0010@auburn.edu>
2014-02-28 10:39:08 -0600

    Added user posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
   	@user = User.new
-  end
-
-  def show
-  	@user = User.find(params[:id])
   end
 
   def create
@@ -54,14 +51,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
-      end
-    end
+    # Before filters    
 
     def correct_user
       @user = User.find(params[:id])
